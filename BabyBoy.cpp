c549cf4be85274e1e392921a0d36f1b5c24a8762
Thiago Valle <thiagoqvalle@gmail.com>
2020-05-04 22:06:15 -0400

    Initial timer logic

--- BabyBoy.cpp
+++ BabyBoy.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <SDL.h>
 #include "Rom.h"
 #include "SoC.h"
 #include "SDLWindow.h"
+
 
 int main(int argc, char* argv[])
 {
@@ -24,9 +26,16 @@ int main(int argc, char* argv[])
 
     SDL_Event e;
 
+    Uint32 timer = 0;
+
+    int frames = 0;
+    int seconds = 0;
+
     bool exit = false;
     while (!exit)
     {
+        timer = SDL_GetTicks();
+
         while (SDL_PollEvent(&e) != 0)
         {
             if (e.type == SDL_QUIT)
@@ -47,6 +56,17 @@ int main(int argc, char* argv[])
         window.handleEvent(e);
         window.renderWrapping(bgMatrix, 0x00, 0x00);*/
         //bgMap.render(bgMatrix, 256, 256);
+
+        while (SDL_GetTicks() - timer < 16.666)
+        {
+        }
+
+        frames++;
+        if (frames >= 60)
+        {
+            frames = 0;
+            printf("%d\n", ++seconds);
+        }
 
         exit = !soc.isRunning;
     }
